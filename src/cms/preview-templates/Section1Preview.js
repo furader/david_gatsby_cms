17bdd662c15c653d00b10cb6158580e33d468547
unknown <furaders@gmail.com>
2020-10-07 02:19:09 +0200

    aaa

--- src/cms/preview-templates/Section1Preview.js
+++ src/cms/preview-templates/Section1Preview.js
@@ -1,10 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Section1Template } from '../../templates/section1-page'
+import { graphql, useStaticQuery } from 'gatsby'
 
- const Section1Preview = ({ entry, getAsset }) => {
+ const Section1Preview = async({ entry, getAsset }) => {
   const data = entry.getIn(['data']).toJS()
+  var t = entry.getIn(['data', 'background_image'])
+  var im =  getAsset(entry.getIn(['data', 'background_image']));
 
+  const nn = await useStaticQuery(graphql`
+ query{ markdownRemark(frontmatter: {identifier: {eq: "section1"}}) {
+    frontmatter {
+      background_image {
+        childImageSharp {
+          fluid (maxWidth: 3000,quality: 100){
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      title
+      subtitle
+      identifier
+      category
+      booking
+      achievements {
+        text
+      }
+    }
+  }}
+     
+  `)
+
+  data.background_image = nn.markdownRemark.frontmatter.background_image;
   if (data) {
     return (
       <Section1Template
